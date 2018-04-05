3878591eb4d8e35df2940ff048f5811a2eea5c58
github username <github email address>
2018-04-05 13:28:35 -0400

    done

--- src/components/hogIndex.js
+++ src/components/hogIndex.js
@@ -4,21 +4,33 @@ import hogs from '../porkers_data';
 
 class HogIndex extends React.Component {
 
+
+
   generateHog = () => {
     return hogs.map((hog) => {
-         return hog
-        console.log(hog.name)
+      return <div className="ui grid container" >
+        <h2 onClick={ this.handleHogClick } className="ui eight wide column"> { hog.name }</h2>
+    </div>
+    })
+  }
+
+  generateHogInfo = () => {
+    return hogs.map((hog) => {
+      return <div className="ui eight wide column" id={ hog.name } onClick={ this.handleHogClick } >
+        <p>{ hog.specialty }</p>
+        <p>{ hog.greased }</p>
+        <p>{ hog['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'] }</p>
+        <p>{ hog['highest medal achieved'] }</p>
+      </div>
     })
   }
 
   render(){
-    const hogsZ = this.generateHog()
-    console.log(hogsZ)
+    const hogListDetails = this.generateHogInfo()
+    const hogList = this.generateHog()
     return (
         <div>
-          <h1>
-          { hogsZ.name }
-        </h1>
+          { (this.state.click) ? hogListDetails : hogList }
         </div>
 
     )
